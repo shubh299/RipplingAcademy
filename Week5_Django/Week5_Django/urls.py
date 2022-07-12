6497791fe623284664bb5a49c98ff006995e70fa
Shubham Singh <shubhamsingh@rippling.com>
2022-07-12 18:10:17 +0530

    Redefined directory names

--- Week5_Django/Week5_Django/urls.py
+++ Week5_Django/Week5_Django/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""Week5_Django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
