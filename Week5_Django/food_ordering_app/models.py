50b7429793724ab2ee928bcc26613543cd3961a3
Shubham Singh <shubhamsingh@rippling.com>
2022-07-12 18:11:42 +0530

    Defined models for food-ordering-app

--- Week5_Django/food_ordering_app/models.py
+++ Week5_Django/food_ordering_app/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+from mongoengine import *
 
-# Create your models here.
+user_types = ('app_admin', 'restaurant_manager', 'customer')
+
+
+class User(Document):
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True)
+    type = StringField(choices=user_types)
+
+
+class Restaurant(Document):
+    name = StringField(required=True)
+    address = StringField(required=True)
+    cuisines = ListField()
+    logo = ImageField()  # this needs to be changed to url or storage location for future
+    manager = ReferenceField(User, reverse_delete_rule=CASCADE)
+
+
+dish_category = ()
+
+
+class Dish(Document):
+    name = StringField(required=True)
+    description = StringField()
+    dish_image = ImageField()
+    is_veg = BooleanField()
+    category = StringField(choices=dish_category)
+    availability = BooleanField(default=False)
+    restaurant_from = ReferenceField(Restaurant)
+    # availability_duration = ListField()
+
+
+class Order(Document):
+    order_id = SequenceField(unique=True)
+    restaurant_name = ReferenceField(Restaurant)
+    dishes_ordered = DictField()  # format --> dish_name : quantity
+    ordered_by = ReferenceField(User)
