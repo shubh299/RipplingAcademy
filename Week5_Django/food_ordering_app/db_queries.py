be292d84f45678557623c223587c8e65eeafbf80
Shubham Singh <shubhamsingh@rippling.com>
2022-07-14 19:09:11 +0530

    Changed query to be case insensitive

--- Week5_Django/food_ordering_app/db_queries.py
+++ Week5_Django/food_ordering_app/db_queries.py
@@ -4,15 +4,15 @@ from food_ordering_app.models import User, Restaurant, Dish
 
 
 def get_restaurant_from_db(restaurant_name: str, restaurant_address: str) -> QuerySet:
-    restaurant_in_db = Restaurant.objects(Q(name=restaurant_name) & Q(address=restaurant_address))
+    restaurant_in_db = Restaurant.objects(Q(name__iwholeword=restaurant_name) & Q(address__iwholeword=restaurant_address))
     return restaurant_in_db
 
 
 def search_dish_in_restaurant(restaurant: Restaurant, dish_name: str):
-    dish = Dish.objects(Q(name=dish_name) & Q(restaurant_from=restaurant))
+    dish = Dish.objects(Q(name__iwholeword=dish_name) & Q(restaurant_from=restaurant))
     return dish
 
 
 def get_user_from_db_by_email(email: str) -> QuerySet:
-    users_in_db = User.objects(email=email)
+    users_in_db = User.objects(email__iwholeword=email)
     return users_in_db
